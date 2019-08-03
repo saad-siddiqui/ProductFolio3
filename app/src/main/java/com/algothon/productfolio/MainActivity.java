e82053b8c3cdf9e283b9a856595577ab4e1408e4
Saad Siddiqui <saad.2000.siddiqui@gmail.com>
2019-08-03 08:43:03 +0530

    Added button to main activity to show welcome toast to user

--- app/src/main/java/com/algothon/productfolio/MainActivity.java
+++ app/src/main/java/com/algothon/productfolio/MainActivity.java
@@ -3,6 +3,8 @@ package com.algothon.productfolio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SayHello(View view) {
+        Toast.makeText(this, "Welcome to Product Folio", Toast.LENGTH_SHORT).show();
     }
 }
